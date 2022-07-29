56ba2ce3f93516a7deba620a20eb15a2e413cb03
DESKTOP-VJFBLT5\Juan <juanes_2002-13@hotmail.com>
2022-07-28 21:15:06 -0500

    no message

---
+++ login-app/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ClienteController extends Controller
+{
+    public function registerClient(Request $request){
+        // Se crea el usuario con los datos del registro
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // Le asignamos el rol de Cliente
+        $user->assignRole('cliente');
+    }
+}
